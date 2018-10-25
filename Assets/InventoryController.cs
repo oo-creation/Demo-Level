2083cc8ecb93cdf8da34d353682f10be197b526e
OO-Creation <oo-creation@outlook.com>
2018-10-25 11:43:48 +0200

    Added inventory and rocks

---
+++ Assets/InventoryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryController : MonoBehaviour
+{
+
+	public List<Image> InventorySlots;
+	public Image TextBox;
+	public Text TextBoxText;
+
+	private void Awake()
+	{
+		InventorySlots = new List<Image>();
+	}
+
+	private void Start()
+	{
+		TextBox.enabled = false;
+		TextBoxText.text = "";
+	}
+
+	private void Update()
+	{
+		
+	}
+}
