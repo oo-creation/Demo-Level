7065085d72421ed54ff4fc6a79c504295172b47e
OO-Creation <oo-creation@outlook.com>
2018-10-25 15:57:20 +0200

    Added ai to shark, rotation not working

---
+++ Assets/Scripts/FishSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+	public Transform SpwawnPoint;
+	public GameObject FishPrefab;
+
+	private void Start()
+	{
+		StartCoroutine(SpawnFishes()	);
+	}
+
+	private IEnumerator SpawnFishes()
+	{
+		Debug.Log("Start Coroutine");
+		Instantiate(FishPrefab, SpwawnPoint);
+		yield return new WaitForSeconds(Random.Range(1f, 5f));
+		
+	}
+}
---
+++ Assets/Scripts/SwimController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwimController : MonoBehaviour
+{
+	public float MaxSpeed;
+	public float WanderRadius;
+	public Transform TargetPoint;
+
+	private float _wanderPoint = 0f;
+	private float _wanderSpeed = .05f;
+	
+	private void Update()
+	{
+		var velocity = MoveToTarget() + Wander();
+		velocity.y = transform.position.y;
+		transform.position = velocity;
+	}
+
+	private Vector3 MoveToTarget()
+	{
+		var targetPos = Vector3.MoveTowards(transform.position, TargetPoint.position, MaxSpeed * Time.deltaTime);
+		return targetPos;
+	}
+
+	private Vector3 Wander()
+	{
+		if (_wanderPoint < 0)
+			_wanderSpeed += WanderRadius * Time.deltaTime;
+		else
+			_wanderSpeed -= WanderRadius * Time.deltaTime;
+
+		_wanderPoint += _wanderSpeed;
+		return _wanderPoint * transform.right;
+	}
+}
